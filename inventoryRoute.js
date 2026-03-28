ddd21617604f48a8a57e4bd9aafe994a5fed1b63
mejaelek <mejaelekamohelo@gmail.com>
2026-03-29 00:47:19 +0200

    Refactor inventoryRoute.js for cleaner routing

--- inventoryRoute.js
+++ inventoryRoute.js
@@ -3,14 +3,7 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
 
-router.get(
-  "/type/:classificationId",
-  utilities.handleErrors(invController.buildByClassificationId)
-)
-
-router.get(
-  "/detail/:inv_id",
-  utilities.handleErrors(invController.buildByInventoryId)
-)
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId))
 
 module.exports = router
