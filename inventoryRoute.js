2b32011e2e8e35f14eff815e4fcb4a8fd0bb6343
mejaelek <mejaelekamohelo@gmail.com>
2026-03-29 00:59:28 +0200

    Delete inventoryRoute.js

--- inventoryRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = new express.Router()
-const invController = require("../controllers/invController")
-const utilities = require("../utilities/")
-
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
-router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId))
-
-module.exports = router
