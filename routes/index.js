905752cdb43d524916ee0afda4d344be38e6b7fb
mejaelek <mejaelekamohelo@gmail.com>
2026-03-28 21:49:31 +0200

    Remove comments and clean up index.js

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = new express.Router()
 const baseController = require("../controllers/baseController")
 const utilities = require("../utilities/")
 
-// Route to build home view
 router.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Intentional error route — Task 3
 router.get(
     "/error",
     utilities.handleErrors(baseController.triggerError)
 )
 
-module.exports = router 
+module.exports = router
